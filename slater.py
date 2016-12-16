37c4293dd05235ef6e1cce906a894d9fe442bbe2
Marty McGuire <schmartissimo@gmail.com>
2016-12-15 23:49:44 -0500

    Move HTML into j2 templates

--- slater.py
+++ slater.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import Flask, redirect, request, session, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_micropub import MicropubClient
 
 app = Flask(__name__)
@@ -17,18 +17,7 @@ def login_required(f):
 
 @app.route('/')
 def index():
-    return """
-    <!DOCTYPE html>
-    <html>
-      <body>
-        <form action="/authorize" method="GET">
-          <input type="text" name="me" placeholder="http://example.com" />
-          <input type="hidden" name="scope" value="post" />
-          <button type="submit">Sign In</button>
-        </form>
-      </body>
-    </html>
-    """
+    return render_template('index.jinja2')
 
 @app.route('/authorize')
 def authorize():
@@ -50,31 +39,7 @@ def new_event():
     me = session.get('_micropub_me')
     endpoint = session.get('_micropub_endpoint')
     access_token = session.get('_micropub_access_token')
-    return """
-    <!DOCTYPE html>
-    <html>
-      <body>
-        <p>Posting as {}</p>
-        <form method="POST" action="{}" enctype="multipart/form-data">
-          <input type="hidden" name="access_token" value="{}" />
-          <input type="hidden" name="h" value="event" />
-          <div><label for="name">Event Name</label><input type="text" name="name" /></div>
-          <!-- TODO: date/time picker w/ TZ -->
-          <div><label for="start">Start Date/Time</label><input type="text" name="start" placeholder="2016-12-06 19:30-05:00" /></div>
-          <!-- TODO: date/time picker w/ TZ -->
-          <div><label for="end">End Date/Time</label><input type="text" name="end" placeholder="2016-12-06 21:30-05:00" /></div>
-          <!-- TODO: venue picker -->
-          <div><label for="location">Location</label><input type="text" name="location" /></div>
-          <!-- TODO: split on commas, strip whitespace, make into category[] on submit -->
-          <div><label for="category">Categories</label><input type="text" name="category" /></div>
-          <div><label for="photo">Poster Image</label><input type="file" name="photo" /></div>
-          <div><label for="content">Description</label><div><textarea name="content"></textarea></div></div>
-          <div><input type="submit" value="Create Event" /></div>
-        </form>
-      </body>
-    </html>
-    """.format(me, endpoint, access_token)
-
+    return render_template('new.jinja2', me=me, endpoint=endpoint, access_token=access_token)
 
 if __name__ == '__main__':
     app.run(debug=True)
