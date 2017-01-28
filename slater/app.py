34794de9f1d6dc8e0f87674577ca24a4dc72c9a0
Marty McGuire <schmartissimo@gmail.com>
2017-01-28 16:24:36 -0500

    Refactor app into blueprint and exts

---
+++ slater/app.py
@@ -0,0 +1,24 @@
+from . import extensions
+from .views import views
+from flask import Flask
+import logging
+import sys
+
+def create_app(config_path='../slater.cfg'):
+    app = Flask('slater')
+    app.config.from_pyfile(config_path)
+    configure_logging(app)
+    extensions.init_app(app)
+    app.register_blueprint(views)
+    return app
+
+def configure_logging(app):
+    if app.debug:
+        return
+
+    app.logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
